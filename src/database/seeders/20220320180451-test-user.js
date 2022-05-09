5c510471dc3b903c32829820ea64592edc67e2f4
ArouayAb <arouayab@gmail.com>
2022-05-09 16:36:17 +0100

    basic seeding functions added

--- src/database/seeders/20220320180451-test-user.js
+++ src/database/seeders/20220320180451-test-user.js
@@ -1,5 +1,7 @@
 'use strict';
 
+
+const { faker } = require('@faker-js/faker');
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -13,105 +15,122 @@ module.exports = {
      */
 
     async function customInsert(tableName, records, options) {
-      let userIds = [];
+      let ids = [];
       let index = await queryInterface.bulkInsert(tableName, records, options);
 
       do{
-        userIds.push(index);
+        ids.push(index);
         index++;
       }while(index <= users.length);
 
-      return userIds;
+      return ids;
     }
 
-    const users = [
-      {
-        email: 'email@email.com',
-        password: 'JoeMama',
-        phone_number: '0646464644',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        email: 'email2@email.com',
-        password: 'DenyWordhehe',
-        phone_number: '0646464644',
-        createdAt: new Date(),
-        updatedAt: new Date()
+    function generateUsers(number) {
+      let users = [];
+      for(let index = 1; index < number; index++) {
+        users.push({
+          id: index,
+          email: faker.internet.email(),
+          password: faker.internet.password(),
+          phone_number: faker.phone.phoneNumber("+212 6 ## ## ## ##"),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
       }
-    ];
-    let userIds = [];
-    userIds = await customInsert('Users', users, {});
+      return users;
+    }
 
-
-    let profilesList = [];
-    for (const userId of userIds) {
-      let profiles = [
-        {
-          name: 'profile1',
-          wallet_id: 'ididididiidididididiididid',
-          picture_url: 'sdjofhosdjf.com/dfsdf',
-          banner_url: 'sdjofhosdjf.com/sdklfsdj',
-          acc_creation_date: new Date(),
+    function generateProfiles(users) {
+      let profiles = [];
+      for(const user of users) {
+        profiles.push({
+          id: user.id,
+          name: faker.name.findName(),
+          wallet_id: faker.datatype.uuid(),
+          picture_url: faker.internet.url(),
+          banner_url: faker.internet.url(),
+          acc_creation_date: faker.date.past(),
           createdAt: new Date(),
           updatedAt: new Date(),
-          UserId: userId
-        }
-      ];
-      // profilesList.push(await customInsert('Profiles', profiles, {}));
-      await customInsert('Profiles', profiles, {})
+          UserId: user.id
+        })
+      }
+      return profiles;
     }
 
-    // let customOffersList = [];
-    // for (const profileIds of profilesList) {
-    //   for (const profileId of profileIds) {
-    //     let customOffers = [
-    //       {
-    //         id: profileId,
-    //         title: 'title1',
-    //         body: 'body1',
-    //         value_offered: 354.12,
-    //         creation_date: '1999-12-12',
-    //         createdAt: new Date(),
-    //         updatedAt: new Date(),
-    //         ProfileId: profileId
-    //       },
-    //       {
-    //         id: profileId,
-    //         title: 'title2',
-    //         body: 'body2',
-    //         value_offered: 954.12,
-    //         creation_date: '2009-12-12',
-    //         createdAt: new Date(),
-    //         updatedAt: new Date(),
-    //         ProfileId: profileId
-    //       }
-    //     ];
-    //     customOffersList.push(await customInsert('CustomOffers', customOffers, {}));
-    //   }
-    // }
-    //
-    // for (const customOfferIds of customOffersList) {
-    //   for (const customOfferId of customOfferIds) {
-    //     let attachments = [
-    //       {
-    //         name: 'name1',
-    //         attachment_url: 'dsoudsfuohsd@fds.com',
-    //         createdAt: new Date(),
-    //         updatedAt: new Date(),
-    //         CustomOfferId: customOfferId
-    //       },
-    //       {
-    //         name: 'name2',
-    //         attachment_url: 'ddsfuohsd@fds.fr',
-    //         createdAt: new Date(),
-    //         updatedAt: new Date(),
-    //         CustomOfferId: customOfferId
-    //       }
-    //     ];
-    //     await customInsert('CustomOffers', attachments, {});
-    //   }
-    // }
+    function generateCustomOffers(profiles, numberPerProfile) {
+      let customOffers = [];
+      for(const profile of profiles) {
+        for(let index = numberPerProfile * (profile.id - 1) + 1; index <= numberPerProfile * profile.id; index++) {
+          customOffers.push({
+            id: index,
+            title: faker.company.catchPhrase(),
+            body: faker.lorem.text(),
+            value_offered: faker.datatype.number(),
+            creation_date: faker.date.past(),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            ProfileId: profile.id
+          })
+        }
+      }
+      return customOffers;
+    }
+
+    function generateNfts(profiles, customOffers, numberPerProfile) {
+      let nfts = [];
+
+      for(const profile of profiles) {
+
+        let ownedCustomOffers = [];
+        for(const customOffer of customOffers) {
+          if (customOffer.ProfileId === profile.id) {
+            ownedCustomOffers.push(customOffer);
+          }
+        }
+
+        for (let index = numberPerProfile * (profile.id - 1) + 1; index <= numberPerProfile * profile.id; index++) {
+
+          function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+          }
+
+          let randomOwnedCustonOfferIndex = getRandomInt(0, ownedCustomOffers.length - 1);
+          let customOfferExist = getRandomInt(0, 2);
+
+          nfts.push({
+            id: index,
+            creation_date: faker.date.past(),
+            contract_adress: faker.address.city(0),
+            token_id: faker.datatype.uuid(),
+            description: faker.lorem.text(),
+            name: faker.datatype.string(),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            ProfileId: profile.id,
+            CustomOfferId: customOfferExist === 1 ? ownedCustomOffers[randomOwnedCustonOfferIndex].id : undefined
+          })
+          //console.log(ownedCustomOffers[randomOwnedCustonOfferIndex]);
+          ownedCustomOffers.splice(randomOwnedCustonOfferIndex, 1);
+        }
+      }
+      return nfts;
+    }
+
+    let users = generateUsers(10);
+    await customInsert('Users', users, {});
+
+    let profiles = generateProfiles(users);
+    await customInsert('Profiles', profiles, {});
+
+    let customOffers = generateCustomOffers(profiles, 3);
+    await customInsert('CustomOffers', customOffers, {});
+
+    let nfts = generateNfts(profiles, customOffers, 2);
+    await customInsert('Nfts',nfts, {});
 
   },
 
@@ -123,6 +142,7 @@ module.exports = {
      * await queryInterface.bulkDelete('People', null, {});
      */
 
+    await queryInterface.bulkDelete('Nfts', null, {});
     await queryInterface.bulkDelete('OfferAttachments', null, {});
     await queryInterface.bulkDelete('CustomOffers', null, {});
     await queryInterface.bulkDelete('Profiles', null, {});
