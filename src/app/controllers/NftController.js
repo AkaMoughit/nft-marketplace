15a9d6805d9568ed6b08660a37e4ef53ce80ea13
ArouayAb <arouayab@gmail.com>
2022-05-09 21:34:43 +0100

    test list all added

--- src/app/controllers/NftController.js
+++ src/app/controllers/NftController.js
@@ -3,7 +3,7 @@ const nftService = require("../services/NftService");
 exports.nftsPage = function(request, response) {
     nftService.listAll()
         .then(data => {
-            response.status(200).render('all_nfts', { result: JSON.stringify(data) });
+            response.status(200).render('all_nfts', { result: data });
         }).catch(err => {
             response.status(404).send(err);
         });
