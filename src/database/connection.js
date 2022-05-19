23e10faf062eed76563d4245c2f96bdcb511b0d0
ArouayAb <arouayab@gmail.com>
2022-05-19 19:07:27 +0100

    refactoring

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 const {development} = require("../configs/db.config");
 
 module.exports = dbConnect = () => {
-    var sequelize = new Sequelize(development.database, development.username, development.username, {
+    var sequelize = new Sequelize(development.database, development.username, development.password, {
         host: development.host,
         dialect: development.dialect
     });
@@ -12,7 +12,7 @@ module.exports = dbConnect = () => {
             await sequelize.authenticate();
         }
         console.log('Connection to MySQL database established');
-
+        return sequelize;
     } catch (error) {
         console.error('Unable to connect to database:', error);
     }
