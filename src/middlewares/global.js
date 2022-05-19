b5bbea345207bba62e427c6a87280d3fc3e7a50f
UB <75035882+ArouayAb@users.noreply.github.com>
2022-05-19 18:32:59 +0100

    Update global.js

--- src/middlewares/global.js
+++ src/middlewares/global.js
@@ -2,6 +2,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const express = require("express");
+const session = require('express-session');
+const SessionStore = require('connect-session-sequelize')(session.Store);
 
 module.exports = appMiddlewares = (app) => {
     app.use(cors());
@@ -9,6 +11,18 @@ module.exports = appMiddlewares = (app) => {
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(bodyParser.json());
     app.use(express.static('./src/public'));
+    var sessionStore = new SessionStore({
+    db: sequelize,
+    });
+
+    app.use(session({
+        resave : sessionCfg.resave,
+        saveUninitialized : sessionCfg.saveUnitialized,
+        secret : sessionCfg.secret,
+        store : sessionStore
+    }))
+
+    sessionStore.sync();
     app.use(require('../routes'));
     app.use(function(req, res){ res.status(404).render('404', { url: req.originalUrl }); });
 }
