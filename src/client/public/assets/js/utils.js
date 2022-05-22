f68c5be4fd1155ec924e99dd9f4b1703d15a62d2
ArouayAb <arouayab@gmail.com>
2022-05-22 23:05:24 +0100

    contract events can be triggered now

--- src/client/public/assets/js/utils.js
+++ src/client/public/assets/js/utils.js
@@ -33,6 +33,7 @@ $("#digitalArtRadioId").on('click', () => {
 });
 
 $(".testSendSigner").on("click", async () => {
+
     let accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
 
     var provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -42,14 +43,29 @@ $(".testSendSigner").on("click", async () => {
     let Marketplace = await (await fetch("../contracts/Marketplace.json")).json();
     let MarketplaceAddress = await (await fetch("../contracts/Marketplace-address.json")).json();
 
-    let marketplaceContract = new ethers.Contract(MarketplaceAddress, Marketplace.abi, signer);
+    let Nft = await (await fetch("../contracts/NFT.json")).json();
+    let NftAddress = await (await fetch("../contracts/NFT-address.json")).json();
 
-    $.ajax({
-        url: 'http://localhost:3000/',
-        type: 'get',
-        success: function (res) {
-            window.location.href = "index";
-        }
+    let marketplaceContract = new ethers.Contract(MarketplaceAddress, Marketplace.abi, signer);
+    let nftContract = new ethers.Contract(NftAddress, Nft.abi, signer);
+
+    const toWei = (number) => ethers.utils.parseEther(number.toString());
+
+    nftContract.on('Minted', (a, b, c) => {
+        console.log(a.toString(), b, c);
     });
+
+    let tokenId = await nftContract.mint("URI HERE TEST");
+
+    await nftContract.setApprovalForAll(marketplaceContract.address, true);
+    await marketplaceContract.makeItem(nftContract.address, (await nftContract.tokenCount()).toString(), toWei(2));
+
+    // $.ajax({
+    //     url: 'http://localhost:3000/',
+    //     type: 'get',
+    //     success: function (res) {
+    //         window.location.href = "index";
+    //     }
+    // });
 
 });
