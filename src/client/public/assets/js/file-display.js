6a5cddbc9911e900f103402f7adc337bd48533f7
ArouayAb <arouayab@gmail.com>
2022-06-05 15:44:11 +0100

    Added audio and video support to Nfts

--- src/client/public/assets/js/file-display.js
+++ src/client/public/assets/js/file-display.js
@@ -1,12 +1,16 @@
 function getFileType(fileUrl) {
     return new Promise(async (resolve, reject) => {
-        fileUrl = await (await fetch(fileUrl)).blob();
-        const reader = new FileReader();
-        reader.readAsDataURL(fileUrl);
-        reader.onloadend = () => {
-            let fileType = reader.result.split(";")[0].split(":")[1];
-            resolve(fileType);
-        };
+        let file = (await fetch(fileUrl));
+        let fileBlob = await file.blob();
+        resolve(fileBlob.type)
+
+        // const reader = new FileReader();
+        // reader.readAsDataURL(fileBlob);
+        // reader.onloadend = () => {
+        //     // console.log(reader.result);
+        //     let fileType = reader.result.split(";")[0].split(":")[1];
+        //     resolve(fileType);
+        // };
     })
 }
 
@@ -15,9 +19,13 @@ $(document).ready(async function () {
     for (let file of files) {
         let fileUrl = file.dataset.src;
         let fileType = await getFileType(fileUrl);
+
+        console.log(fileType);
+
         let unknownUrl = ['application/octet-stream'];
         let imageValidUrl = ['image/png', 'image/jpeg', 'image/svg', 'image/webp'];
-        let videoValidUrl = ['video/mp4', 'video/ogg', 'video/webm'];
+        let videoValidUrl = ['video/mp4', 'video/ogg', 'video/webm', 'video/x-matroska'];
+        let audioValidUrl = ['audio/mpeg'];
 
         if(imageValidUrl.includes(fileType)) {
             let image = document.createElement("img");
@@ -28,10 +36,17 @@ $(document).ready(async function () {
             let video = document.createElement("video");
             video.setAttribute("src", fileUrl);
             video.setAttribute("alt", file);
-            video.setAttribute("controls", "")
+            video.setAttribute("controls", "");
             video.style.cssText += 'width: 100%;';
             video.style.cssText += 'height: 100%;';
             file.appendChild(video);
+        } else if(audioValidUrl.includes(fileType)) {
+            let audio = document.createElement(("audio"));
+            audio.setAttribute("src", fileUrl);
+            audio.setAttribute("alt", file);
+            audio.setAttribute("controls", "");
+            audio.style.cssText += 'width: 100%;';
+            file.appendChild(audio);
         } else if(unknownUrl.includes(fileType)){
             let object = document.createElement("object");
             object.setAttribute("data", fileUrl);
